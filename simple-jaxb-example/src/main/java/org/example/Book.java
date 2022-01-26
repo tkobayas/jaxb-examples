dc26967425971a7a729382027c6b7175634a43c7
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2022-01-26 19:10:02 +0900

    simple example

---
+++ simple-jaxb-example/src/main/java/org/example/Book.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@XmlRootElement(name = "book")
+@XmlType(propOrder = {"id", "name", "date"})
+public class Book {
+
+    private Long id;
+    private String name;
+    private String author;
+    private Date date;
+
+    @XmlAttribute
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @XmlElement(name = "title")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlTransient
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @XmlJavaTypeAdapter(DateAdapter.class)
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", name=" + name + ", author=" + author + ", date=" + date + "]";
+    }
+
+}
---
+++ simple-jaxb-example/src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class Main {
+
+    public static void main(String[] args) throws JAXBException, IOException {
+        Main main = new Main();
+        main.marshal();
+        
+        Book book = main.unmarshall();
+        System.out.println(book);
+    }
+
+    public void marshal() throws JAXBException, IOException {
+        Book book = new Book();
+        book.setId(1L);
+        book.setName("Book1");
+        book.setAuthor("Author1");
+        book.setDate(new Date());
+
+        JAXBContext context = JAXBContext.newInstance(Book.class);
+        Marshaller mar = context.createMarshaller();
+        mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        mar.marshal(book, new File("./book.xml"));
+    }
+
+    public Book unmarshall() throws JAXBException, IOException {
+        JAXBContext context = JAXBContext.newInstance(Book.class);
+        return (Book) context.createUnmarshaller()
+                             .unmarshal(new FileReader("./book.xml"));
+    }
+}
